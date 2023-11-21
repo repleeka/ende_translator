61bc363e2b31d971e85b0b28fc040fd0c14b5a00
repleeka <tungondugi@gmail.com>
2023-11-22 00:30:12 +0530

    full website template along with the translator page

--- app.py
+++ app.py
@@ -8,22 +8,46 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     title = "Home"
-    return render_template('index.html', title=title)
+    return render_template('home.html', title=title)
 
 
-reversed_text = []
+@app.route('/about')
+def about():
+    title = "About"
+    return render_template("about.html", title=title)
 
 
-@app.route('/reverse', methods=['POST', 'GET'])
-def reverse():
-    title = "Reverse"
-    text = request.form.get('textToTranslate')
-    if not text:
-        return render_template('index.html',  title=title)
-    else:
-        # appending and reversing the latest entry in the list
-        reversed_text.append("{}".format(text[::-1]))
-        return render_template('index.html', reversed_text=reversed_text, title=title)
+@app.route('/screenshot')
+def screenshot():
+    title = 'Screenshot'
+    return render_template('screenshot.html', title=title)
+
+
+@app.route('/pricing')
+def pricing():
+    title = "Pricing"
+    return render_template("pricing.html", title=title)
+
+
+@app.route('/subscribe')
+def subscribe():
+    title = "Newsletter"
+    return render_template('subscribe.html', title=title)
+
+
+# reversed_text = []
+
+
+# @app.route('/reverse', methods=['POST', 'GET'])
+# def reverse():
+#     title = "Reverse"
+#     text = request.form.get('textToTranslate')
+#     if not text:
+#         return render_template('index.html',  title=title)
+#     else:
+#         # appending and reversing the latest entry in the list
+#         reversed_text.append("{}".format(text[::-1]))
+#         return render_template('index.html', reversed_text=reversed_text, title=title)
 
 
 @app.route('/translate', methods=['POST', 'GET'])
@@ -34,21 +58,21 @@ def translate():
     dest = 'ja'
     src_text = request.form.get('textToTranslate')
     if not src_text:
-        return render_template('index.html', title=title)
+        return render_template('translator.html', title=title)
     else:
         # translating the text
         dest_text = translator.translate(src_text, dest=dest, src=src).text
         dest_pronunciation = translator.translate(
             src_text, dest=dest, src=src).pronunciation
-        return render_template('index.html', dest_text=dest_text, dest_pronunciation=dest_pronunciation, title=title)
+        return render_template('translator.html', dest_text=dest_text, dest_pronunciation=dest_pronunciation, title=title)
 
 
-@app.route('/clear', methods=['POST', 'GET'])
-def clear_list():
-    title = "Clear"
-    txt = reversed_text.clear()
-    return render_template('index.html', txt=txt, title=title)
+# @app.route('/clear', methods=['POST', 'GET'])
+# def clear_list():
+#     title = "Clear"
+#     txt = reversed_text.clear()
+#     return render_template('index.html', txt=txt, title=title)
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+# if __name__ == "__main__":
+#     app.run(debug=False, host='0.0.0.0')
