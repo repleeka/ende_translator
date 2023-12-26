02a03eb113c375968c8777c8b5875b74e1df636c
repleeka <tungondugi@gmail.com>
2023-12-26 19:44:19 +0530

    machine translation model implemented

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, make_response
-from googletrans import Translator, LANGUAGES
+import sentencepiece as spm
+import nltk
+import ctranslate2
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -15,74 +17,56 @@ def about():
     return render_template("about.html", title=title)
 
 
+def trans(source, translator, sp_source_model, sp_target_model):
+    """Use CTranslate model to translate a sentence
+        Args:
+            source (str): Source sentences to translate
+            translator (object): Object of Translator, with the CTranslate2 model
+            sp_source_model (object): Object of SentencePieceProcessor, with the SentencePiece source model
+            sp_target_model (object): Object of SentencePieceProcessor, with the SentencePiece target model
+        Returns:
+            Translation of the source text
+    """
+    source_sentences = nltk.sent_tokenize(source)
+    source_tokenized = sp_source_model.encode(
+        source_sentences, out_type=str)
+    translations = translator.translate_batch(source_tokenized)
+    translations = [translation[0]["tokens"]
+                    for translation in translations]
+    translations_detokenized = sp_target_model.decode(translations)
+    translation = " ".join(translations_detokenized)
+
+    return translation
+
+
+# [Modify] File paths here to the CTranslate2 SentencePiece models.
+ct_model_path = "ende_ct2"
+sp_source_model_path = "models/sentencepiece.model"
+sp_target_model_path = "models/sentencepiece.model"
+
+# Create objects of CTranslate2 Translator and SentencePieceProcessor to load the models
+translator = ctranslate2.Translator(
+    ct_model_path, "cpu")    # or "cuda" for GPU
+sp_source_model = spm.SentencePieceProcessor(sp_source_model_path)
+sp_target_model = spm.SentencePieceProcessor(sp_target_model_path)
+
+
 @app.route('/translate', methods=['POST', 'GET'])
 def translate():
-    # Set the page title
-    title = "Translator"
+    # ct2-opennmt-py-converter --model_path model.pt --output_dir ct2_model
 
-    # Get the list of supported languages
-    langs = LANGUAGES
-
-    # Create the Google Translate API translator object
-    translator = Translator()
-
-    # Retrieve the value of the 'selected_src_language' cookie from the request,
-    # defaulting to 'en' if the cookie is not present
-    active_src_language = request.cookies.get(
-        'selected_src_language', default='en')
-
-    # Retrieve the value of the 'selected_dest_language' cookie from the request,
-    # defaulting to 'ja' if the cookie is not present
-    active_dest_language = request.cookies.get(
-        'selected_dest_language', default='ja')
+    title = "English to German Translator"
 
     if request.method == "POST":
-
-        # Get the source language code from the form
-        src_code = request.form.get('src_language')
-
-        # Get the text to translate from the form
-        src_text = request.form.get('textToTranslate')
-
-        # Get the destination language code from the form
-        dest_code = request.form.get('dest_language')
-
-        # Translate the text using the codes
-        dest_text = translator.translate(
-            src_text, dest=dest_code, src=src_code).text
-
-        # Get the pronunciation for the translated text
-        dest_pronunciation = translator.translate(
-            src_text, dest=dest_code, src=src_code).pronunciation
-
-        # Set the selected language in a cookie that expires in 30 days (you can adjust the expiration)
-        resp = make_response(render_template(
-            'translator.html',
-            langs=langs,
-            dest_text=dest_text,
-            dest_pronunciation=dest_pronunciation,
-            title=title,
-            src_text=src_text,
-            active_src_language=src_code,
-            active_dest_language=dest_code
-        ))
-        # Set the source language cookie with a value of src_code and a maximum age of 30 days
-        resp.set_cookie('selected_src_language', value=src_code,
-                        max_age=30*24*60*60)  # 30 days expiration
-
-        # Set the destination language cookie with a value of dest_code and a maximum age of 30 days
-        resp.set_cookie('selected_dest_language', value=dest_code,
-                        max_age=30*24*60*60)  # 30 days expiration
-
-        # Return the response object to render the 'translator.html' page with translated text and other details
-        return resp
-
-    else:
-        # Handle the GET request, render the page
-        return render_template(
-            'translator.html',
-            title=title,
-            langs=langs,
-            active_src_language=active_src_language,
-            active_dest_language=active_dest_language
-        )
+        user_input = request.form.get('textToTranslate')
+        if not user_input:
+            error = "Please enter text to translate."
+            return render_template('translator.html', title=title, error=error)
+        try:
+            translation = trans(user_input, translator,
+                                sp_source_model, sp_target_model)
+            return render_template('translator.html', translation=translation, title=title, user_input=user_input)
+        except Exception as e:
+            error = "Translation error: {}".format(e)
+            return render_template('translator.html', error=error, title=title)
+    return render_template('translator.html', title=title)
